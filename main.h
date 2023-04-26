38be54b5a7df00b22edbe19e1a3225641bccba0c
khaoula-berrah <berah.khaoula@gmail.com>
2023-04-25 17:58:40 -0700

    testttt

--- main.h
+++ main.h
@@ -7,14 +7,14 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* Flags */
+/* FLAGS */
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* Sises */
+/* SIZES */
 #define S_LONG 2
 #define S_SHORT 1
 
@@ -32,7 +32,7 @@ struct fmt
 
 
 /**
- * typedef struct fmt fmt_t - Struct op.
+ * typedef struct fmt fmt_t - Struct op
  *
  * @fmt: The format.
  * @fm_t: The function associated.
@@ -43,12 +43,9 @@ int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
-<<<<<<< HEAD
-/*_________________ FUNCTIONS ____________*/
-=======
->>>>>>> db79f6a04f9d64ad8edcea72917a02ffb07f58a6
+/*************** Functions ******************/
 
-/*________print chars and strings _____________*/
+/* print chars and strings */
 int print_char(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -78,7 +75,7 @@ int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* Funcion to print memory address */
-int print_pointer(va_list types, char buffer[], 
+int print_pointer(va_list types, char buffer[],
 	int flags, int width, int precision, int size);
 
 /* handle other specifiers */
@@ -109,7 +106,7 @@ int write_unsgnd(int is_negative, int ind,
 char buffer[],
 	int flags, int width, int precision, int size);
 
-/*___________________ UTILS ______________________*/
+/****************** UTILS ******************/
 int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
@@ -117,4 +114,4 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-#endif /***** MAIN_H ****/
+#endif /* MAIN_H */
